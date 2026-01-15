7d914cd2b69d7526b2a089986a55231d97b263ea
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2026-01-15 21:45:20 +0600

    Add SQL query to replace Employee ID with unique identifier

---
+++ new/Replace_Employee_ID_With_The_Unique_Identifier.sql
@@ -0,0 +1,6 @@
+// problem link: https://leetcode.com/problems/replace-employee-id-with-the-unique-identifier/description/?envType=study-plan-v2&envId=top-sql-50
+
+select unique_id,name
+from Employees
+left join EmployeeUNI
+on Employees.id = EmployeeUNI.id;
