cbc0649e373e49cef6392787fc30ec3a78b0e670
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2026-01-22 00:19:39 +0600

    Create  Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql

---
+++ new/Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,8 @@
+// Problem Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/?envType=study-plan-v2&envId=top-sql-50
+
+select v.customer_id, count(v.visit_id) as count_no_trans
+from Visits as v
+left join Transactions as t
+on v.visit_id = t.visit_id
+where t.transaction_id is null
+group by v.customer_id;
